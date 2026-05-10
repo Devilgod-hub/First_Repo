0eba16d9cb64d490b3e9e143c9ad4df8f528b69a
Nischal Karmacharya <58777852+nischal-devz@users.noreply.github.com>
2026-05-11 02:22:20 +0545

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,9 +2,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 public class Main{
         public static void main(String[] args){
-        Date todaysdate = new Date();
-        SimpleDateFormat FormattedDate = new SimpleDateFormat("dd-MM-yyyy");
-        System.out.println("This is a normal java app that shows current Date: " +FormattedDate.format(todaysdate));
-        System.out.println("Commit=jenkins build");
+        Date var1 = new Date();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+
+        String date = dateFormat.format(var1);
+        String time = timeFormat.format(var1);
+
+        System.out.println("This is a normal java app that shows current Date: " + date);
+        System.out.println("This is current Time: " + time);
         }
 }
